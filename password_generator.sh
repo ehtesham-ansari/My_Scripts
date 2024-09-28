9f5fe901641053572f6c18c967d9f0c1cd3f7583
Md Ehtesham <168437259+ehtesham-ansari@users.noreply.github.com>
2024-09-28 11:56:31 +0530

    Update password_generator.sh

--- password_generator.sh
+++ password_generator.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-############################
+###################################
 # Purpose: simple password generator
 # author: Md Ehtesham
 # version: 1.0.0
 ###################################
 
-echo "please enter the desired length of password"
+echo "Please enter the desired password length (max 15):"
 read pass_length
 
 # checking if the provided argument is a number or not.
-if ! expr "$1" + 0 >/dev/null 2>&1; then
-    echo "Error: Argument is not a number."
+if ! expr "$pass_length" + 0 >/dev/null 2>&1; then
+    echo "Invalid input. The Input could only be in numbers."
     exit 1
 fi
 
@@ -19,6 +19,6 @@ if [ "$pass_length" -gt 15 ]; then
     echo "the password length cannot exceed 15 charactors"
     exit 1
 else
-        password=openssl rand -base64 48 | cut -c1-$pass_length
+        password=$(openssl rand -base64 48 | cut -c1-$pass_length)
         echo "generated password is: $password"
 fi
